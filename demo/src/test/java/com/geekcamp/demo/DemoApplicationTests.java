a560e4235628d160fc7a6dd2e0664c97c3493ab3
longbq <longbq@yinhai.com>
2022-05-15 23:00:34 +0800

    week02

---
+++ demo/src/test/java/com/geekcamp/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.geekcamp.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
